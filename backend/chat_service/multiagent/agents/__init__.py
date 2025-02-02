00bc818aef8c0872c9bd896300d2d756009b37ea
dissian <mikerk92@gmail.com>
2025-02-01 21:37:53 -0600

    updated Agent NAme

--- backend/chat_service/multiagent/agents/__init__.py
+++ backend/chat_service/multiagent/agents/__init__.py
@@ -1,5 +1,5 @@
 from .librarian_agent import librarian
-from .prc_government_expert import expert_subgraph_entry, expert_subgraph_report, collab_subgraph_entry
+from .Expert_Agent import expert_subgraph_entry, expert_subgraph_report, collab_subgraph_entry
 from .synthesis_agent import synthesis_agent
 from .user_proxy_moderator import get_Moderator_Guidance
 from .conversation_history_manager import conversation_history_manager
